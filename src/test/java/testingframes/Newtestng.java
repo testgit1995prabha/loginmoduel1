6e70832e9294db857febf5a06672e712c25a5360
testgit1995prabha <testingdemo1995prabha@gmail.com>
2022-01-04 17:28:41 +0530

    login module done

---
+++ src/test/java/testingframes/Newtestng.java
@@ -0,0 +1,75 @@
+package testingframes;
+
+import java.util.Date;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Newtestng {
+
+	public WebDriver driver;
+	
+	@BeforeClass
+	private void DateandTime() {
+
+		Date d = new Date();
+		System.out.println(d);
+
+		
+	}
+	
+	@BeforeMethod
+	private void Browserlauch() {
+
+		WebDriverManager.chromedriver().setup();
+		 driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	private void tc1() {
+
+		driver.get("https://www.facebook.com/");
+
+		WebElement mail = driver.findElement(By.id("email"));
+		mail.sendKeys("selenium");
+		
+			}
+	
+	@Test
+	private void tc2() {
+
+		driver.get("https://www.snapdeal.com/");
+		
+		WebElement search = driver.findElement(By.name("keyword"));
+		search.sendKeys("iphone");
+	}
+	
+    @AfterMethod	
+	private void enddate() {
+
+		Date d = new Date();
+		System.out.println(d);
+		
+	}
+	
+    @AfterClass
+	private void browserclose() {
+
+		driver.close();
+		System.out.println("closed");
+	}
+	
+	
+	
+	
+}
